f82ced487341f806a1d1d08f7580cc65979eeb87
conno234 <135058788+conno234@users.noreply.github.com>
2024-04-08 23:01:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
-from flask import Flask, jsonify
-import psycopg2 
-import os
+from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
-# Parameters to connect to the database
+# Database parameters
 db_params = {
-    'database': os.environ.get("DB_DATABASE"),  
-    'user': os.environ.get("DB_USER"),  
-    'password': os.environ.get("DB_PASSWORD"), 
-    'host': os.environ.get("DB_HOST"),  
-    'port': os.environ.get("DB_PORT") 
+    'database': 'lab1.2',  
+    'user': 'postgres',  
+    'password': 'IMissPinole1312!?', 
+    'host': '34.16.107.82',  
+    'port': '5432' 
 }
 
-def get_column_headers(table_name, db_params):
-    conn = psycopg2.connect(**db_params)
-    cur = conn.cursor()
-    cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
-    column_headers = [row[0] for row in cur.fetchall()]
-    conn.close()
-    return column_headers
+def fetch_grid_codes():
+    conn = None
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**db_params)
+        
+        # Create a cursor
+        cursor = conn.cursor()
+        
+        # Query to fetch all grid codes with geometries converted to WKT
+        query = "SELECT ST_AsText(geom) FROM landcover_shp"
+        
+        # Execute the query
+        cursor.execute(query)
+        
+        # Fetch all rows
+        rows = cursor.fetchall()
+        
+        # Close cursor
+        cursor.close()
+        
+        # Return grid codes
+        return [row[0] for row in rows]
+    except (Exception, psycopg2.DatabaseError) as error:
+        return str(error)
+    finally:
+        # Close the connection
+        if conn is not None:
+            conn.close()
 
 @app.route('/')
-def show_column_headers():
-    table_name = "landcoverpoint_shp"
-    column_headers = get_column_headers(table_name, db_params)
-    return jsonify(column_headers)
+def index():
+    # Call the function to fetch grid codes
+    grid_codes = fetch_grid_codes()
+    if isinstance(grid_codes, list):
+        return '<br>'.join(grid_codes)
+    else:
+        return grid_codes
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
