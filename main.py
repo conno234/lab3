3b7a77dcfca6424d14049105f51e26cf34470280
conno234 <135058788+conno234@users.noreply.github.com>
2024-04-08 22:44:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def get_grid_codes():
 
 if __name__ == '__main__':
     # Run Flask app
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=8080)
