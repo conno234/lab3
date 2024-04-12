e99359ec0900d5474f63879c97cbc26c9c482592
conno234 <135058788+conno234@users.noreply.github.com>
2024-04-11 20:15:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import os
 app = Flask(__name__)
 
 db_params = {
+    #'database': 'lab3',  
+    #'user': os.environ.get("DB_USER"),  
+    #'password': os.environ.get("DB_PASSWORD"), 
+    #'host': os.environ.get("DB_HOST"),  
+    #'port': os.environ.get("DB_PORT") 
     'database': 'lab3',  
-    'user': os.environ.get("DB_USER"),  
-    'password': os.environ.get("DB_PASSWORD"), 
-    'host': os.environ.get("DB_HOST"),  
-    'port': os.environ.get("DB_PORT") 
+    'user': 'postgres',  
+    'password': 'IMissPinole1312!?', 
+    'host': '34.16.107.82',  
+    'port': '5432' 
 }
 
 def fetch_geom_as_geojson(table_name, geom_column, db_params):
