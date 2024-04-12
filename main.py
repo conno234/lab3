9f21a1925140b66c71d27de99f401d0fc4478181
conno234 <135058788+conno234@users.noreply.github.com>
2024-04-11 20:18:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def fetch_geom_as_geojson(table_name, geom_column, db_params):
         #return geojson_without_slashes
     #except (psycopg2.Error, json.JSONDecodeError) as e:
         # Handle exceptions gracefully, log or return appropriate error response
-     return geojson_with_slashes
+    return geojson_with_slashes
 
 @app.route('/kriging_point')
 def get_geojson():
